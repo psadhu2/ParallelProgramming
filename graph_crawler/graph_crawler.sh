5b35432b9ce0a61280bcb4ec182063e946d240b4
Pranav Aditya Sadhu <sadhu.pranavadi@gmail.com>
2026-02-17 20:18:20 -0500

    centarus 3

--- graph_crawler/graph_crawler.sh
+++ graph_crawler/graph_crawler.sh
@@ -7,28 +7,15 @@
 SOURCE_FILE="bfs.cpp"
 EXECUTABLE="bfs.exe"
 
-# Print what we're doing
 echo "Compiling $SOURCE_FILE..."
-echo "Arguments: Actor='$1', Depth='$2'"
+echo "Actor: $ACTOR"
+echo "Depth: $DEPTH"
 
-# Compile with error checking
 g++ "$SOURCE_FILE" -I. -o "$EXECUTABLE" -lcurl
 
-# Check if compilation succeeded
 if [ $? -ne 0 ]; then
     echo "Compilation failed!"
     exit 1
 fi
 
-echo "Compilation successful. Running program..."
-
-# Run with arguments
-./"$EXECUTABLE" "$1" "$2"
-
-# Check exit status
-if [ $? -ne 0 ]; then
-    echo "Program execution failed!"
-    exit 1
-fi
-
-echo "Program completed successfully."
+./"$EXECUTABLE" "$ACTOR" "$DEPTH"
