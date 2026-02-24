f1fbe9071eb49320222439050116827c01e8bedd
Pranav Aditya Sadhu <sadhu.pranavadi@gmail.com>
2026-02-24 18:04:12 -0500

    Parallel Graph Traversal

---
+++ sequential/level_client_script.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+#SBATCH --job-name=pranav-benchmark
+#SBATCH --partition=Centaurus
+#SBATCH --mem=10G
+#SBATCH --time=00:15:00
+#SBATCH --output=output_%j.txt
+#SBATCH --cpus-per-task=16
+
+make
+
+./level_client "$1" "$2"
